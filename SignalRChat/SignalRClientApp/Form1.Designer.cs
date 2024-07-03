457ea5e95bdc5ca8f3e5a421deb1e873e516d45d
zhcngu <42114771+zhcngu@users.noreply.github.com>
2024-07-03 16:56:51 +0800

    client and server

---
+++ SignalRChat/SignalRClientApp/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace SignalRClientApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUserMsg = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtMsgs = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(108, 219);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(185, 62);
+            this.btnlogin.TabIndex = 14;
+            this.btnlogin.Text = "登录";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(108, 480);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(185, 62);
+            this.btnSend.TabIndex = 13;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "消息";
+            // 
+            // btnConn
+            // 
+            this.btnConn.Enabled = false;
+            this.btnConn.Location = new System.Drawing.Point(108, 356);
+            this.btnConn.Name = "btnConn";
+            this.btnConn.Size = new System.Drawing.Size(185, 62);
+            this.btnConn.TabIndex = 11;
+            this.btnConn.Text = "链接";
+            this.btnConn.UseVisualStyleBackColor = true;
+            this.btnConn.Click += new System.EventHandler(this.btnConn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "用户";
+            // 
+            // txtUserMsg
+            // 
+            this.txtUserMsg.Location = new System.Drawing.Point(108, 73);
+            this.txtUserMsg.Multiline = true;
+            this.txtUserMsg.Name = "txtUserMsg";
+            this.txtUserMsg.Size = new System.Drawing.Size(311, 66);
+            this.txtUserMsg.TabIndex = 9;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(108, 30);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(311, 22);
+            this.txtUser.TabIndex = 8;
+            this.txtUser.Text = "winform";
+            // 
+            // txtMsgs
+            // 
+            this.txtMsgs.Location = new System.Drawing.Point(545, 2);
+            this.txtMsgs.Name = "txtMsgs";
+            this.txtMsgs.Size = new System.Drawing.Size(507, 659);
+            this.txtMsgs.TabIndex = 15;
+            this.txtMsgs.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1167, 718);
+            this.Controls.Add(this.txtMsgs);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnConn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUserMsg);
+            this.Controls.Add(this.txtUser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUserMsg;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.RichTextBox txtMsgs;
+    }
+}
+
---
+++ SignalRChat/SignalRClientApp/Form1.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SignalRClientApp
+{
+    public partial class Form1 : Form
+    {
+        HubConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+            var host = ConfigurationManager.AppSettings["signalrHost"];
+            connection = new HubConnectionBuilder()
+    .WithUrl($"{host}/ChatHub", ops => { ops.AccessTokenProvider = async () => { return await Task.FromResult(token); }; })
+    .Build();
+            connection.Closed += async (error) =>
+            {
+                //await connection.DisposeAsync();
+                // connection=null;
+
+                //await Task.Delay(new Random().Next(0, 5) * 1000);
+                //await connection.StartAsync();
+                this.Invoke(new Action(() => {
+                    btnConn.Enabled = true;
+                    txtMsgs.Text += "服务端断开链接\r\n";
+                }));
+                await Task.CompletedTask;
+            };
+            connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                this.Invoke(new Action(() =>
+                {
+                    var newMessage = $"{DateTime.Now.ToString()} {user}: {message}";
+                    txtMsgs.Text += newMessage + "\r\n";
+                }));
+            });
+        }
+        string token = "";
+        private async void btnlogin_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                var host = ConfigurationManager.AppSettings["signalrHost"];
+                var url = $"{host}/home/login?user={txtUser.Text}";
+                HttpClient client = new HttpClient()
+                {
+                };
+                token = await client.GetStringAsync(url);
+                txtMsgs.Text += token + "\r\n";
+                btnConn.Enabled = true;
+                btnlogin.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                txtMsgs.Text += "登录失败" + ex.Message + "\r\n";
+            }
+        }
+
+        private async void btnConn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await connection.StartAsync();
+                txtMsgs.Text += "Connection started" + "\r\n";
+                await connection.InvokeAsync("Join",
+                txtUser.Text, "winform");
+                btnConn.Enabled = false;
+                btnSend.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                txtMsgs.Text += ex.Message + "\r\n";
+            }
+        }
+
+        private async void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await connection.InvokeAsync("SendMessage",
+                    txtUser.Text, txtUserMsg.Text);
+            }
+            catch (Exception ex)
+            {
+                txtMsgs.Text += ex.Message + "\r\n";
+            }
+        }
+    }
+}
